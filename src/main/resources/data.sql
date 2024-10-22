8252f10aaa3d2e015d3a98ba1df83cb994047995
vincentarck <vincenthambaya@gmail.com>
2024-10-22 15:39:41 +0700

    chore:copy project app remote

--- src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO missions
-    (agent, title, gadget1, gadget2)
-VALUES
-    ('Johnny English', 'Rescue the Queen',
-    'Exploding Cigar', 'Voice Controlled Rolls Royce'),
-    
-    ('Natasha Romanova', 'Kill Iron Man',
-    'Armored Suit', 'Indestructible Pole');
